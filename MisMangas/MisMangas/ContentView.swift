79b94213fa9dbf44bc3b3cd897ef0ad2a06ddadf
Juan Ferrera Sala <ferrerasalajuan@outlook.es>
2025-12-20 11:44:11 +0100

    Initial commit

---
+++ MisMangas/MisMangas/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MisMangas
+//
+//  Created by Juan Ferrera Sala on 20/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MisMangas/MisMangas/MisMangasApp.swift
@@ -0,0 +1,17 @@
+//
+//  MisMangasApp.swift
+//  MisMangas
+//
+//  Created by Juan Ferrera Sala on 20/12/25.
+//
+
+import SwiftUI
+
+@main
+struct MisMangasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
